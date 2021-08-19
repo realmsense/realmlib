89e015bdf262ef2745dfe22e6a8f58774b90c60b
Extacy <git@extacy.cc>
2021-08-19 21:06:15 +1200

    Add ActivateType, PetFamily, SlotType

---
+++ src/constants/pet-family.ts
@@ -0,0 +1,17 @@
+export enum PetFamily
+{
+    Aquatic,
+    Automaton,
+    Avian,
+    Canine,
+    Exotic,
+    Farm,
+    Feline,
+    Humanoid,
+    Insect,
+    Penguin,
+    Reptile,
+    Spooky,
+    Woodland,
+    "? ? ? ?"
+}
---
+++ src/constants/slot-type.ts
@@ -0,0 +1,32 @@
+/**
+ * Used in objects.xml to describe what the type of an item is
+ */
+export enum SlotType {
+    Sword =         1,
+    Dagger =        2,
+    Bow =           3,
+    Tome =          4,
+    Shield =        5,
+    LeatherArmor =  6,
+    HeavyArmor =    7,
+    Wand =          8,
+    Ring =          9,
+    Consumable =    10,
+    Spell =         11,
+    Seal =          12,
+    Cloak =         13,
+    Robe =          14,
+    Quiver =        15,
+    Helm =          16,
+    Staff =         17,
+    Poison =        18,
+    Skull =         19,
+    Trap =          20,
+    Orb =           21,
+    Prism =         22,
+    Scepter =       23,
+    Katana =        24,
+    Star =          25,
+    PetEgg =        26,
+    Wakizashi =     27
+}
